6d1802afda9bbbed35ffffb156b300d94fccd2d3
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-15 11:59:25 +0530

    Added Income Tax

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -20,18 +20,26 @@ namespace Methods
             //float balance = DepositAmount(10000, 1000);
 
             //string str = Console.ReadLine();
-            var str = Console.ReadLine(); //Implicitly typed variable
+            //var str = Console.ReadLine(); //Implicitly typed variable
 
-            var balance = DepositAmount(
-                 amount: 10000 /*Named Parameter*/
-                , intrestRate: 10
-                , initialBalance: 1000);
+            //var balance = DepositAmount(
+            //     amount: 10000 /*Named Parameter*/
+            //    , intrestRate: 10
+            //    , initialBalance: 1000);
 
             //DepositAmount(10000, 10, 1000);
 
-            Console.WriteLine($"Current Balance is {balance}");
-        }
+            //Console.WriteLine($"Current Balance is {balance}");
 
+            Console.Write("Enter your annual income : ");
+            var userInput = Console.ReadLine();
+            var annualIncome = int.Parse(userInput);
+
+            var taxPercentage   = CalculateTax(annualIncome);
+            var totalTaxPayable = CalculateTaxAmount(annualIncome, taxPercentage);
+
+            DisplayTaxInformation(annualIncome,taxPercentage,totalTaxPayable);
+        }
         #region simplemethos
 
         /*
@@ -53,12 +61,33 @@ namespace Methods
 
         //1. Optional parameters should come only after all regular parameters
 
-        static float DepositAmount(float amount
-            , float initialBalance = 0 /*Optional Parameter*/
-            , float intrestRate = 0.0F /*Optional Parameter*/)
+        //static float DepositAmount(float amount
+        //    , float initialBalance = 0 /*Optional Parameter*/
+        //    , float intrestRate = 0.0F /*Optional Parameter*/)
+        //{
+        //    var currentBalance = initialBalance + amount;
+        //    return currentBalance;
+        //}
+        static float CalculateTax(int annualIncome)
         {
-            var currentBalance = initialBalance + amount;
-            return currentBalance;
+            float tax = 0.0F;
+
+            if (annualIncome >= 200000 && annualIncome < 500000)
+                tax = .10F;
+            else if (annualIncome >= 500000 && annualIncome < 700000)
+                tax = .20F;
+            else if (annualIncome >= 700000)
+                tax = .30F;
+
+            return tax;
+        }
+        static float CalculateTaxAmount(int annualIncome, float tax)
+        {
+            return annualIncome * tax;
+        }
+        static void DisplayTaxInformation(int annualIncome, float tax, float taxAmount)
+        {
+            Console.WriteLine($"Annual Income : {annualIncome}, Tax Per : {tax}, Tax Amount : {taxAmount}");
         }
     }
 }
