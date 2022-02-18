ac584a88ee167b7a21b18e32109bf3ad760edd6a
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-18 15:51:22 +0530

    Added Employee

---
+++ EmployeeTax/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTax
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Others
+    }
+
+    public  class Employee
+    {
+        public int id;
+        public string name;
+        public Gender gender;
+        public float annualincome;
+
+        public float CalculateTax()
+        {
+            float tax = 0.0F;
+            float minTaxAmount = gender == Gender.Male ? 200000 : 250000;
+
+            if (minTaxAmount >= 200000 && annualincome < 500000)
+                tax = .10F;
+            else if (annualincome >= 500000 && annualincome < 700000)
+                tax = .20F;
+            else if (annualincome >= 700000)
+                tax = .30F;
+
+            return tax;
+        }
+
+        public float CalculateTaxAmount()
+        {
+            float tax   = CalculateTax();
+            return annualincome * tax;
+        }
+
+        public void DisplayDetails()
+        {
+            float taxPer    = CalculateTax();
+            float taxAmount = CalculateTaxAmount();
+
+            Console.WriteLine($@"Id : {id}
+                                 Name : {name}
+                                 Gender : {gender}
+                                 Annual Income : {annualincome}
+                                 Tax Per : {taxPer}
+                                 Tax Amount : {taxAmount}");
+        }
+    }
+}
---
+++ EmployeeTax/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1     = new Employee();
+            emp1.name         = "Sreeharui";
+            emp1.annualincome = 500000;
+            emp1.gender       = Gender.Male;
+            emp1.id           = 1;
+
+            Employee emp2     = new Employee();
+            emp2.name         = "Kate";
+            emp2.annualincome = 700000;
+            emp2.gender       = Gender.Female;
+            emp2.id           = 2;
+
+            emp1.DisplayDetails();
+            emp2.DisplayDetails();
+
+
+        }
+    }
+}
