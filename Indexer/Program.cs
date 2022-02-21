c74c301fa6ba12a8ab3635a6be01ca9f4158a4c6
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-21 12:39:23 +0530

    Added Indexer

---
+++ Indexer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WeekDays weekdays = new WeekDays();
+            string weekDay    = weekdays[0];
+            int weekDayIndex = weekdays["Monday"];
+
+            weekdays[0] = "MONDAY";
+
+            //string weekDay = weekdays.GetWeekDayByIndex(0);
+            Console.WriteLine(weekDay);
+            Console.WriteLine(weekDayIndex);
+
+        }
+    }
+}
---
+++ Indexer/WeekDays.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    public class WeekDays
+    {
+        string[] weekDays = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+        //Indexer
+        //Special Property that will have a "this"
+        //insted of a property name
+
+        //Pro vs Indexer
+        //1. Property can have a name, Indexer takes this
+        //2. Property cannot have a parameter, Indexer can have one or more param
+        public string this[int index]
+        {
+            get
+            {
+                return weekDays[index];
+            }
+             set
+            {
+                weekDays[index] = value;
+            }
+        }
+
+        public int this[string weekDay]
+        {
+            get
+            {
+                int weekDayIndex = 0;
+                for (int i = 0; i < weekDays.Length; i++)
+                    if (weekDay.Equals(weekDays[i], StringComparison.OrdinalIgnoreCase))
+                        weekDayIndex = i;
+
+                return weekDayIndex;
+            }
+        }
+
+        //public string GetWeekDayByIndex(int index)
+        //{
+        //    return weekDays[index];
+        //}
+    }
+}
