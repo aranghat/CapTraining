6178a8007dbb92283d43efdc4784ae0efc4b7b6c
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-21 17:20:01 +0530

    Overloading

---
+++ OverloadingMethods/Account.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingMethods
+{
+    public class Account
+    {
+        public int AccountId      { get; set; }
+        public string AccountType { get; set; }
+        public string Name        { get; set; }
+        public int CurrentBalance { get; private set; }
+
+        //Having same Methods with diffrent signatures is called
+        //Method overloading
+        public int DepositAmount(int depositAmount)
+        {
+            CurrentBalance += depositAmount;
+            return CurrentBalance;
+        }
+
+        public int DepositAmount(int depositAmount
+                , string remarks)
+        {
+            CurrentBalance += depositAmount;
+            Console.WriteLine($"Deposited Amount " +
+                $":{depositAmount}, Remarks: {remarks}");
+            return CurrentBalance;
+        }
+
+        public int DepositAmount(float depositAmount)
+        {
+            CurrentBalance += (int)depositAmount;
+            return CurrentBalance;
+        }
+
+        public static bool operator ==
+                    (Account firstAccount
+                    , Account secondAccount)
+        {
+            bool isSame = firstAccount.AccountId == 
+                                secondAccount.AccountId;
+            return isSame;
+        }
+
+        public static bool operator !=(Account account1
+            , Account account2)
+        {
+            bool isSame = account1.AccountId 
+                                        == account2.AccountId;
+            return isSame;
+        }
+
+        public static Account operator+(Account account1
+                                       ,Account account2)
+        {
+            Account account = new Account();
+            account.Name = account1.Name;
+            account.CurrentBalance = account1.CurrentBalance 
+                                        + account2.CurrentBalance;
+
+            return account;
+        }
+
+    }
+}
---
+++ OverloadingMethods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+            account.Name = "Sreehari Aranghat";
+            account.AccountId = 10001;
+            account.AccountType = "Savings";
+
+            account.DepositAmount(10000);
+            account.DepositAmount(5000, "Intrest pay back");
+
+            Console.WriteLine(account.CurrentBalance);
+
+            Account account1    = new Account();
+            account1.Name        = "Sreehari Aranghat";
+            account1.AccountId   = 10001;
+            account1.AccountType = "Savings";
+
+            account1.DepositAmount(10000);
+            account1.DepositAmount(5000, "Intrest pay back");
+
+            if (account == account1)
+                Console.WriteLine("Both are same account");
+            else
+                Console.WriteLine("Both are different Accounts");
+
+            Account consolidatedAccount = account + account1;
+            Console.WriteLine($"Total amount in bank " +
+                $"{consolidatedAccount.CurrentBalance}");
+
+        }
+    }
+}
