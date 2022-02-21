8e0f6ecc6e539ab915843c83a47424ec659691d4
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-21 16:32:23 +0530

    Added sample for inheritance

---
+++ InheritanceBasics/Employee.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace InheritanceBasics
+{
+    //Base Class
+    public abstract class Employee
+    {
+        public int EmployeeID   { get; set; }
+        public string Name      { get; set; }
+        public float BasicPay   { get; set; }
+
+        //1. It does not have a implementation
+        //2. Every child class has to provide their own implementation
+        public abstract void DisplayDetails();
+
+        public virtual float CalculateSalary()
+        {
+            float totalSalary = BasicPay - (BasicPay * .20F);
+            return totalSalary;
+        }
+
+        public void DisplaySalary()
+        {
+            var totalSalary = CalculateSalary();
+            Console.WriteLine(
+                $"Before Tax : {BasicPay}" +
+                $"Inhand Salary : {totalSalary}");
+        }
+    }
+
+    //Child Class
+    public class JuniorSoftwareEngineer
+        : Employee
+    {
+        public override void DisplayDetails()
+        {
+            Console.WriteLine($@"NAME : {Name}
+                                DESIGNATION : JSE
+                                SALARY : {BasicPay}");
+        }
+    }
+    public class SeniorSoftwareEngineer 
+        : Employee {
+
+        public override void DisplayDetails()
+        {
+            Console.WriteLine($@"NAME : {Name}
+                                DESIGNATION : SSE
+                                SALARY : {BasicPay}");
+        }
+    }
+    public class ProjectManager : Employee
+    {
+        public float ProjectBonus { get; set; }
+        public override float CalculateSalary()
+        {
+            var baseSalary  = base.CalculateSalary();
+            var totalSalary = baseSalary + ProjectBonus;
+
+            return totalSalary;
+        }
+
+        public override void DisplayDetails()
+        {
+            Console.WriteLine($@"NAME : {Name}
+                                DESIGNATION : PROJECT MANAGER
+                                SALARY : {BasicPay}
+                                PROJECT BONUS : {ProjectBonus}");
+        }
+
+        //Method Hiding
+        public new void DisplaySalary()
+        {
+            var totalSalary = CalculateSalary();
+            Console.WriteLine(
+                $"Before Tax : {BasicPay}" +
+                $"Inhand Salary : {totalSalary}" +
+                $"Project Bonus : {ProjectBonus}");
+        }
+    }
+
+    public sealed class SeniorProjectManager : ProjectManager
+    {
+
+    }
+
+}
+
+
---
+++ InheritanceBasics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //You cannot create the instance of a abstract class
+            //Employee employee = new Employee();
+
+            JuniorSoftwareEngineer juniorSoftware 
+                                    = new JuniorSoftwareEngineer();
+            juniorSoftware.Name                   = "Sreehari";
+            juniorSoftware.EmployeeID             = 10001;
+            juniorSoftware.BasicPay               = 10000;
+
+            Console.WriteLine(juniorSoftware.CalculateSalary());
+            juniorSoftware.DisplayDetails();
+            juniorSoftware.DisplaySalary();
+
+            ProjectManager projectManager   
+                                    = new ProjectManager();
+            projectManager.BasicPay         = 50000;
+            projectManager.EmployeeID       = 10002;
+            projectManager.Name             = "Steve";
+            projectManager.ProjectBonus     = 25000;
+
+            Console.WriteLine(projectManager.CalculateSalary());
+            projectManager.DisplayDetails();
+            projectManager.DisplaySalary();
+        }
+    }
+}
