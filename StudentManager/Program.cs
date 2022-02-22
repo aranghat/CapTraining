df438d6faa8917eb313369b6743347290b605824
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-22 15:01:27 +0530

    Added Sample Student Info Collector

---
+++ StudentManager/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student [] students = new Student[2];
+
+            for(int i = 0;i< 2;i++)
+            {
+                Student student   = new Student();
+                
+                student.StudentId = StudentIdGenerator.Id;
+                student.Name      = Console.ReadLine();
+
+                students[i] = student;
+            }
+
+            foreach (Student student in students)
+                student.DisplayDetails();
+        }
+
+       
+    }
+
+    public static class StudentExtentions
+    {
+        public static void DisplayDetails(this Student student)
+        {
+            Console.WriteLine(student.Name);
+            Console.WriteLine(student.StudentId);
+            Console.WriteLine("--------------------------------");
+        }
+    }
+}
---
+++ StudentManager/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StudentManager
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        string name;
+        public string Name { 
+            get 
+            { return name; }
+            set
+            {
+                if (Regex.IsMatch(value, "^[a-zA-Z ]+$"))
+                    name = value;
+                else
+                    throw new Exception("Invalid Student Name");
+            }
+        }
+    }
+
+    public static class StudentIdGenerator
+    {
+        static int id = 1001;
+        public static int Id { get { return id++; } }
+    }
+}
