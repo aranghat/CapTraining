161871118e5f1e3ce7cb2dd6245758263cd69785
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-22 09:43:33 +0530

    Added Static Classes

---
+++ StaticClasses/CarSales.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClasses
+{
+    public static class GlobalCarsSales
+    {
+        //Members inside a static class must also be static
+        public static int GlobalCarsSold { get; private set; }
+        public static void UpdateSaleCount()
+        {
+            GlobalCarsSold++;
+        }
+    }
+
+    public class CarSales
+    {
+        public int TotalCarsSold { get; private set; }
+
+        public void CarSold(string carName)
+        {
+            TotalCarsSold++;
+
+            //Call to a static class does not need a instance
+            GlobalCarsSales.UpdateSaleCount();
+        }
+    }
+}
---
+++ StaticClasses/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarSales bangloreSales = new CarSales();
+            CarSales mumbaiSales   = new CarSales();
+            CarSales chenneiSales  = new CarSales();
+
+            bangloreSales.CarSold("Swift");
+            bangloreSales.CarSold("Beleno");
+            bangloreSales.CarSold("Swift");
+            bangloreSales.CarSold("Swift");
+            bangloreSales.CarSold("Swift");
+
+            mumbaiSales.CarSold("Swift");
+            mumbaiSales.CarSold("Beleno");
+            mumbaiSales.CarSold("Swift");
+
+            chenneiSales.CarSold("Beleno");
+
+
+            Console.WriteLine($"Total Cars Sold : {bangloreSales.TotalCarsSold}");
+            Console.WriteLine($"Total Cars Sold : {mumbaiSales.TotalCarsSold}");
+            Console.WriteLine($"Total Cars Sold : {chenneiSales.TotalCarsSold}");
+
+            Console.WriteLine($"Total Cars : {GlobalCarsSales.GlobalCarsSold}");
+        }
+    }
+}
