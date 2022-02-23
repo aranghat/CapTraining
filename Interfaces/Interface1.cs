4b10090da2130e7ac45ee2893a8b32e4719d8cd7
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-23 09:13:27 +0530

    Interfaces

---
+++ Interfaces/Interface1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    //1. Insted of class we use InterFace
+    //2. Its a convention to start the name of interface with I
+    //3. The data members of a interface cannot have a access specifier
+    public interface IVideoPlayer
+    {
+        string ModelName { get; set; }
+        void Play();
+        void Stop();
+    }
+
+    public interface IAudioPlayer
+    {
+        void PlayAudio();
+        void StopAudio();
+    }
+
+    //1. All members of the interface need to be provided a implementation 
+    public class VideoPlayer : IVideoPlayer
+    {
+        public string ModelName { get; set; }
+
+        public void Play()
+        {
+            Console.WriteLine("Video Player : Playing Video though Video Player");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Video Player : Stop Video though Video Player");
+        }
+    }
+
+    public class MobilePhone : IVideoPlayer,IAudioPlayer
+    {
+        public string ModelName { get; set; }
+
+        public void Play()
+        {
+            Console.WriteLine("Mobile Phone : Playing Video when user touchers the screen");
+        }
+
+        public void PlayAudio()
+        {
+            Console.WriteLine("Mobile Phone : Playing Audio when user touchers the screen");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Mobile Phone : Stop Video when user touchers the screen");
+        }
+
+        public void StopAudio()
+        {
+            Console.WriteLine("Mobile Phone : Playing Audio when user touchers the screen");
+        }
+    }
+
+    public class Tablet : IVideoPlayer
+    {
+        public string ModelName { get; set; }
+
+        public void Play()
+        {
+            Console.WriteLine("Tablet : Playing Video when user touchers the screen");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Tablet : Stop Video when user touchers the screen");
+        }
+    }
+
+    public class IPodNano : IAudioPlayer
+    {
+        public void PlayAudio()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopAudio()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the file name to play");
+            string fileNAme = Console.ReadLine();
+
+            Console.WriteLine("Pick Your Device");
+            string devideName = Console.ReadLine();
+
+            IVideoPlayer player = GetPlayer(devideName);
+            player.Play();
+        }
+
+        static IVideoPlayer GetPlayer(string deviceName)
+        {
+            IVideoPlayer player = null;
+
+            switch(deviceName)
+            {
+                case "VideoPlayer": player = new VideoPlayer(); break;
+                case "MobilePhone": player = new MobilePhone();  break;
+                case "Tablet"     : player = new Tablet(); break;
+            }
+
+            return player;
+        }
+    }
+}
