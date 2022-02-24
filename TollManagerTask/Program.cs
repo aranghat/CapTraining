9d883f65efc01b4b314493349977c1c5aea3a261
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-24 15:15:30 +0530

    Added TolManager

---
+++ TollManagerTask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollManagerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TollManager tollManager = new TollManager();
+            tollManager.OnTollCollected += TollManager_OnTollCollected;
+        }
+
+        private static void TollManager_OnTollCollected(Vechile vechile, int tollAmount, int totalTollCollected)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TollManagerTask/TollManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TollManagerTask
+{
+    public class TollManager
+    {
+        private int totalTollCollected = 0;
+
+        public delegate void TollCollectionCompleted(Vechile vechile,int tollAmount,int totalTollCollected);
+        public event TollCollectionCompleted OnTollCollected;
+
+        public void CollectToll(Vechile vechile)
+        {
+            int tollAmount = 0;
+            switch(vechile.VechileType)
+            {
+                case VechileType.FourWeeler : tollAmount = 50;break;
+                case VechileType.Cargo      : tollAmount = 100; break;
+            }
+
+            totalTollCollected += tollAmount;
+
+            Thread.Sleep(2000);
+
+            if(OnTollCollected != null)
+                OnTollCollected(vechile,tollAmount,totalTollCollected);
+        }
+    }
+
+    public enum VechileType
+    {
+        TwoWeeler,
+        FourWeeler,
+        Cargo
+    }
+
+    public class Vechile
+    {
+        public string VechileNumber { get; set; }
+        public VechileType VechileType { get; set; }
+    }
+}
