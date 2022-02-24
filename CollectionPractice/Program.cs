a14f8f16258fe8f54865ed2cecfd74c594f82dbf
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-24 09:13:47 +0530

    Added Collection Interfaces

---
+++ CollectionPractice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<Student> students = new List<Student>();
+            SortedSet<Student> students = new SortedSet<Student>();
+
+            Student s1 = new Student { StudentId = 1001, Name = "Sree" };
+            Student s2 = new Student { StudentId = 1002, Name = "Bill" };
+            Student s3 = new Student { StudentId = 1003, Name = "Kate" };
+
+            students.Add(s2);
+            students.Add(s1);
+            students.Add(s3);
+
+            foreach (Student s in students)
+                Console.WriteLine(s.ToString());
+
+            //Student s4 = new Student { StudentId = 1001, Name = "Sree" };
+
+            //if (students.Contains(s4))
+            //    Console.WriteLine("Student is already there in the list");
+            //else
+            //    Console.WriteLine("Student is not there in the list");
+        }
+    }
+}
---
+++ CollectionPractice/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionPractice
+{
+    public class Student : IEquatable<Student>, IComparable<Student>    
+    {
+        public int StudentId { get; set; }
+        public string Name   { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            //1. If the current student is greater
+            //   than the other student return a positve value 1
+
+            //2. If the current student is less
+            //   than the other student return a negative value -1
+
+            //3. If the current student is equal to 
+            //   than the other student return 0
+
+            if(this.StudentId > other.StudentId)
+                return 1;
+            else if(this.StudentId < other.StudentId)
+                return -1;
+            else
+                return 0;
+        }
+
+        public bool Equals(Student other)
+        {
+           return this.StudentId == other.StudentId;
+        }
+
+        public override string ToString()
+        {
+            return $@"Id : {StudentId}, Name {Name}";
+        }
+    }
+}
