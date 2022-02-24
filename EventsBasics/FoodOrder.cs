ddbdf7f7155a32c1f38b76aae4125fa594381c93
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-24 15:04:59 +0530

    Added Events

---
+++ EventsBasics/FoodOrder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventsBasics
+{
+    public class FoodOrder
+    {
+        public delegate void OrderReadyEventHandler(string orderDetails);
+
+        public event OrderReadyEventHandler OrderReady;
+
+        public int Id { get; set; }
+        public string OrderDetails  { get; set; }
+
+
+        public void ProcessOrder()
+        {
+            Console.WriteLine("Starting to create Food");
+            Thread.Sleep(3000);
+
+            //Raise Event
+            OrderReady(OrderDetails);
+        }
+
+        
+    }
+}
---
+++ EventsBasics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FoodOrder order     = new FoodOrder();
+            order.Id            = 1001;
+            order.OrderDetails  = "Double Patty Veg Burger";
+
+            //Subscribing for the event
+            order.OrderReady += Order_OrderReady;
+            order.OrderReady += Order_Shipped;
+            order.ProcessOrder();
+
+            Console.ReadLine();
+        }
+
+        private static void Order_Shipped(string orderDetails)
+        {
+            Console.WriteLine($"{orderDetails} is shipped with the delivery partner");
+        }
+
+        //Event Handler
+        private static void Order_OrderReady(string orderDetails)
+        {
+            Console.WriteLine(orderDetails + " is ready");
+        }
+    }
+}
