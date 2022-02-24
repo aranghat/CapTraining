3ca79f4164a9cdaee67497a4f4bc246d351bbf62
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-24 11:39:51 +0530

    Added Delegates

---
+++ DelegatesAsCallBack/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DelegatesAsCallBack
+{
+    internal class Program
+    {
+        delegate void CallBack(string message);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the file name :");
+            string file = Console.ReadLine();
+
+            Console.WriteLine("Where do you want to play (Mobile/TV/Stereo)");
+            string playWhere = Console.ReadLine();
+
+            CallBack callBack = null;
+
+            switch (playWhere)
+            {
+                case "Mobile": callBack = PlayVideoInMobile;break;
+                case "TV": callBack = PlayVideoInTV; break;
+                case "Stereo": callBack = PlayVideoInStereo; break;
+            }
+
+            DownloadFile(file, callBack);
+        }
+
+        static void DownloadFile(string fileName,CallBack callback)
+        {
+            Console.WriteLine($"Starting to Download File {fileName}");
+            Thread.Sleep(2000);
+            Console.WriteLine("Completed downloading File");
+
+            callback($"Download is completed");
+        }
+
+        static void PlayVideoInTV(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("Playing Video in TV");
+        }
+
+        static void PlayVideoInMobile(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("Playing Video in Mobile");
+        }
+        static void PlayVideoInStereo(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("Playing Video in Stereo");
+        }
+    }
+}
---
+++ DelegatesBasics/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesBasics
+{
+    internal class Program
+    {
+        //Declare a delegate which will have the same signature as the method it intent to point to
+        delegate void EmployeeOperation(Employee employee);
+
+        static void Main(string[] args)
+        {
+            //Stores Values
+            int a = 10;
+            int b = 20;
+
+            int c = a + b;
+
+            //Stores Reference
+            Employee employee = new Employee();
+            employee.Name = "Sree";
+
+            //Stores the pointer to the Method
+            EmployeeOperation scheduleOperation   = ScheduleEmployeeBoardingDate;
+            EmployeeOperation initOperation       = InitiateEmployeeBoardingProcess;
+            EmployeeOperation sentWelcomeKit      = SentWelcomeKit;
+
+            //MultiCaste Delegate
+            EmployeeOperation onBoardEmployee  = scheduleOperation
+                                                + initOperation
+                                                + sentWelcomeKit;
+
+
+            //Ability to treat a method like a variable
+            ProcessEmployeeRecruitment(onBoardEmployee, employee);
+
+            onBoardEmployee = onBoardEmployee - sentWelcomeKit;
+            ProcessEmployeeRecruitment(onBoardEmployee,employee);
+
+
+        }
+
+        static void ProcessEmployeeRecruitment(
+             EmployeeOperation operation // Able to pass the Method as a variable to another method
+            ,Employee employee)
+        {
+            operation(employee);
+        }
+            
+        static void ShowMessage(string message)
+        {
+
+        }
+
+        static void InitiateEmployeeBoardingProcess(Employee employee)
+        {
+            Console.WriteLine($"Initiating Boarding Process For {employee.Name}");
+        }
+
+        static void ScheduleEmployeeBoardingDate(Employee employee)
+        {
+            Console.WriteLine($"Employee Boarding Scheduled for {employee.Name} at March 1 2022");
+        }
+
+        static void SentWelcomeKit(Employee employee)
+        {
+            Console.WriteLine($"Sending Welcome kit with all details to {employee.Name}");
+        }
+    }
+}
