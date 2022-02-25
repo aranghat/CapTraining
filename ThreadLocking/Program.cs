ca43a36a3d5d2671adfdb7082767d7519b507d1e
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-25 11:34:35 +0530

    Added Thread Locking

---
+++ ThreadLocking/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadLocking
+{
+    internal class Program
+    {
+        static List<int > list = new List<int>();
+        static object locker = new object();
+        static void Main(string[] args)
+        {
+            Thread thread1= new Thread(new ThreadStart(ProcessNumbers));
+            Thread thread2 = new Thread(new ThreadStart(ProcessNumbers));
+
+            thread1.Start();
+            thread2.Start();
+        }
+
+        public static void ProcessNumbers()
+        {
+            lock (locker)
+            {
+                //Add numbers to the list 
+                if (list.Count == 0)
+                    for (int i = 0; i < 20; i++)
+                    {
+                        Console.WriteLine(i);
+                        list.Add(i);
+                    }
+            }
+
+            //Add Sum of items in the list
+            if(list.Count > 0)
+            {
+                int totalSum = 0;
+                foreach (int i in list)
+                    totalSum += i;
+
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Total Sum of numbers is : {totalSum}");
+            }
+        }
+    }
+}
---
+++ ThreadingBasics/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread displayNumberThread = new Thread(new ParameterizedThreadStart(DisplayNumbers));
+            Thread downloadFile        = new Thread(new ThreadStart(DownloadFile));
+
+            MyParams myParams = new MyParams { MaxCount = 20, Message = "THREAD 1" };
+
+            displayNumberThread.Start(myParams);
+            downloadFile.Start();
+
+            //DisplayNumbers();
+            //DownloadFile();
+
+        }
+
+        public class MyParams
+        {
+            public string Message { get; set; }
+            public int MaxCount { get; set; }
+        }
+
+        public static void DisplayNumbers(object p)
+        {
+            MyParams myParams = (MyParams)p;
+            for(int i=0;i<myParams.MaxCount;i++)
+            {
+                Console.WriteLine($"{myParams.Message} {i}");
+                Thread.Sleep(100);
+            }
+        }
+
+        public static void DownloadFile()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Download {i}");
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
