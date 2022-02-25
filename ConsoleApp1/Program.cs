f09c0bfcc1415770c4b91efe3f8c0ef1693f9f38
aranghat <58101741+aranghat@users.noreply.github.com>
2022-02-25 16:32:52 +0530

    Added GC

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<10000000000;i++)
+            {
+                Employee employee = new Employee();
+                employee.Id = i;
+                employee.Name = "Sree";
+            }
+
+            Console.ReadLine();
+        }
+
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public byte[] Photo = new byte[1024];
+        }
+    }
+}
