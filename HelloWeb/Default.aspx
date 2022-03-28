dcd8673436997056ebb8c9ff9546f2f4f963b099
aranghat <58101741+aranghat@users.noreply.github.com>
2022-03-28 12:59:06 +0530

    Added Web

---
+++ HelloWeb/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="HelloWeb.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblMessage" runat="server" Text="Hello Web" Font-Bold="True"></asp:Label>
+            <asp:TextBox ID="txtSampleMessage" runat="server" ForeColor="Blue"></asp:TextBox>
+            <asp:Button ID="btnChangeTheMessage" runat="server" 
+                Text="Click to Change the Message" 
+                OnClick="btnChangeTheMessage_Click" />
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="lblPostBackMessage" runat="server" ForeColor="Red" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ HelloWeb/Default.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloWeb
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        /// <summary>
+        /// 1. Create Dynamic Controls
+        /// 2. Set Master Page
+        /// 3. Set the Theme
+        /// </summary>
+        protected void Page_PreInit(object sender,EventArgs e)
+        {
+            Response.Write("1. Pre Init");
+        }
+
+        /// <summary>
+        /// 1.Set custom events to the controls
+        /// 2.Set Properties of a control like background
+        /// </summary>
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Response.Write("2.  Init");
+        }
+
+
+        /// <summary>
+        /// 1. First instance where post back data is available
+        /// </summary>
+        protected void Page_PreLoad(object sender, EventArgs e)
+        {
+             lblPostBackMessage.Text = "<br/>PAGE PRE LOAD";
+        }
+
+
+        /// <summary>
+        /// 1. Used to load data and initialize controls
+        /// 2. React to post back data
+        /// </summary>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblPostBackMessage.Text += "<br />PAGE LOAD";
+            //if(IsPostBack)
+            //{
+            //    lblPostBackMessage.Text = "Post back request";
+            //}
+            //else
+            //{
+            //    lblPostBackMessage.Text = "Newly loaded page";
+            //}
+        }
+
+        protected void Page_PreRender(object sender,EventArgs e)
+        {
+            lblPostBackMessage.Text += "<br />PAGE PRE RENDER";
+        }
+
+        protected void Page_Render(object sender, EventArgs e)
+        {
+            Response.Write("5. Render");
+            lblPostBackMessage.Text += "<br />PAGE RENDER";
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            
+
+            lblPostBackMessage.Text += "<br />PAGE Unload";
+        }
+
+        protected void  btnChangeTheMessage_Click  (object sender, EventArgs e)
+        {
+            lblMessage.Text = txtSampleMessage.Text;
+        }
+    }
+}
