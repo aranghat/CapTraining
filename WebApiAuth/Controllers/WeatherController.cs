7f2b2951227a58305c368028d745b87f5122b8f5
aranghat <58101741+aranghat@users.noreply.github.com>
2022-04-04 14:58:41 +0530

    Added JWT Token handler

---
+++ WebApiAuth/Controllers/WeatherController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApiAuth.Model;
+
+namespace WebApiAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class WeatherController : ControllerBase
+    {
+        static List<WeatherReport> weelyWether = new List<WeatherReport>();
+
+        public WeatherController()
+        {
+            if (weelyWether.Count == 0)
+            {
+                weelyWether.Add(new WeatherReport { WeekDay = "Sunday", Temprature = 32.0F });
+                weelyWether.Add(new WeatherReport { WeekDay = "Monday", Temprature = 27.0F });
+                weelyWether.Add(new WeatherReport { WeekDay = "Tuesday", Temprature = 31.0F });
+                weelyWether.Add(new WeatherReport { WeekDay = "Wednesday", Temprature = 25.0F });
+                weelyWether.Add(new WeatherReport { WeekDay = "Thursday", Temprature = 26.0F });
+                weelyWether.Add(new WeatherReport { WeekDay = "Friday", Temprature = 28.0F });
+            }
+        }
+
+        public IActionResult Get()
+        {
+            return Ok(weelyWether);
+        }
+    }
+}
---
+++ WebApiAuth/Model/WeatherReport.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiAuth.Model
+{
+    public class WeatherReport
+    {
+        public string WeekDay { get; set; }
+        public float Temprature { get; set; }
+    }
+}
