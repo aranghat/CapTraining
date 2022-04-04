e2d5069695115bce154c97db1564ba04f08cc966
aranghat <58101741+aranghat@users.noreply.github.com>
2022-04-04 15:14:39 +0530

    Added Login Controller

---
+++ WebApiAuth/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebApiAuth.Model;
+
+namespace WebApiAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        List<User> users = new List<User>();
+
+        public LoginController()
+        {
+            users.Add(new Model.User { Name = "Sree", UserName = "sreehariis@gmail.com", Password = "12345" });
+            users.Add(new Model.User { Name = "Bill", UserName = "bill@gmail.com", Password = "12345" });
+            users.Add(new Model.User { Name = "Steve", UserName = "steve@gmail.com", Password = "12345" });
+        }
+
+        [HttpPost]
+        public IActionResult Post(User user)
+        {
+            var u = users.FirstOrDefault(d => d.UserName == user.UserName && d.Password == user.Password);
+
+            if (u != null)
+            {
+                var jwtToken = GenerateToken(u.UserName);
+                return Ok(jwtToken);
+            }
+            else
+                return Unauthorized("Login Failed");
+        }
+
+        private string GenerateToken(string userName)
+        {
+            return string.Empty;
+        }
+    }
+}
