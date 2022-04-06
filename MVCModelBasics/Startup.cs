82260e938a44ab3e78eea7359909da0f589c1ea9
aranghat <58101741+aranghat@users.noreply.github.com>
2022-04-06 17:21:28 +0530

    Added card books

--- MVCModelBasics/Startup.cs
+++ MVCModelBasics/Startup.cs
@@ -21,7 +21,8 @@ namespace MVCModelBasics
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app
+            , IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
